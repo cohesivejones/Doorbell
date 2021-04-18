3bc28ea869f1ebe039356551d7c2affbb9e7ea3e
Nathan Jones <njones@stridenyc.com>
2021-04-18 16:52:01 -0400

    Add mqtt support for BLE

--- doorbell.ino
+++ doorbell.ino
@@ -1,18 +1,29 @@
 #include <BLEDevice.h>
 #include <BLEAdvertisedDevice.h>
 #include <WiFi.h>
+#include <PubSubClient.h>
 #include "config.h"
 
+/* Doorbell Service: 00001523-1212-EFDE-1523-785FEABCD123
+ * Buzzer : 00001524-1212-EFDE-1523-785FEABCD123
+ * openDoor : 00001525-1212-EFDE-1523-785FEABCD123
+ */
 static BLEUUID serviceUUID("00001523-1212-EFDE-1523-785FEABCD123");
-static BLEUUID charUUID("00001525-1212-EFDE-1523-785FEABCD123");
-BLERemoteCharacteristic *pCharacteristic;
+static BLEUUID buzzerUUID("00001524-1212-EFDE-1523-785FEABCD123");
+static BLEUUID openDoorUUID("00001525-1212-EFDE-1523-785FEABCD123");
+
+BLEAdvertisedDevice *device;
+
+BLEClient *pClient;
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 bool wifiConnect()
 {
   if (WiFi.status() != WL_CONNECTED)
   {
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.println("Connecting");
+    Serial.println("Attempting WiFi connection");
     while (WiFi.status() != WL_CONNECTED)
     {
       delay(500);
@@ -24,16 +35,55 @@ bool wifiConnect()
   return (WiFi.status() == WL_CONNECTED);
 }
 
+String clientId()
+{
+  String clientId = "ESP32_CLIENT-";
+  clientId += String(random(0xffff), HEX);
+  return clientId;
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  if (pClient)
+  {
+    BLERemoteService *pRemoteService = pClient->getService(serviceUUID);
+    BLERemoteCharacteristic *pRemoteCharacteristic = pRemoteService->getCharacteristic(openDoorUUID);
+    pRemoteCharacteristic->writeValue(1);
+  }
+}
+
+bool mqttConnect()
+{
+  while (!client.connected())
+  {
+    client.setServer(MQTT_SERVER, MQTT_PORT).setCallback(callback);
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect(clientId().c_str(), MQTT_USER, MQTT_PASSWORD))
+    {
+      Serial.println("Connected");
+      return true;
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.println(client.state());
+      delay(500);
+    }
+  }
+  return client.connected();
+}
+
 class MyClientCallback : public BLEClientCallbacks
 {
   void onConnect(BLEClient *pclient)
   {
-    BLERemoteService *pService = pclient->getService(serviceUUID);
-    pCharacteristic = pService->getCharacteristic(charUUID);
   }
 
   void onDisconnect(BLEClient *pclient)
   {
+    client.publish(DOORBELL_INACTIVE, EMPTY_MESSAGE);
+    client.unsubscribe(DOORBELL_BUZZER);
+    delete device;
   }
 };
 
@@ -44,22 +94,13 @@ class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
     if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID))
     {
       BLEDevice::getScan()->stop();
-
-      BLEClient *pClient = BLEDevice::createClient();
-      pClient->setClientCallbacks(new MyClientCallback());
-      pClient->connect(&advertisedDevice);
+      device = new BLEAdvertisedDevice(advertisedDevice);
     }
   }
 };
 
-void setup()
+void Scan()
 {
-  Serial.begin(9600);
-  while (!Serial)
-    ;
-
-  wifiConnect();
-
   BLEDevice::init("");
   BLEScan *pBLEScan = BLEDevice::getScan();
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
@@ -69,6 +110,40 @@ void setup()
   pBLEScan->start(5, false);
 }
 
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial)
+    delay(10);
+
+  Serial.println("Doorbel Client");
+  Serial.println("------------------------------\n");
+
+  wifiConnect();
+  mqttConnect();
+  Scan();
+}
+
 void loop()
 {
+  if (device)
+  {
+    if (!pClient)
+    {
+      Serial.print("Device found: ");
+      Serial.println(device->getName().c_str());
+      pClient = BLEDevice::createClient();
+      pClient->setClientCallbacks(new MyClientCallback());
+      pClient->connect(device);
+      BLERemoteService *pRemoteService = pClient->getService(serviceUUID);
+      BLERemoteCharacteristic *pRemoteCharacteristic = pRemoteService->getCharacteristic(buzzerUUID);
+      notify_callback cb = [](BLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify) {
+        client.publish(DOORBELL_ACTIVE, EMPTY_MESSAGE);
+        delay(3000);
+      };
+      pRemoteCharacteristic->registerForNotify(cb);
+      client.subscribe(DOORBELL_BUZZER);
+    }
+  }
+  client.loop();
 }
